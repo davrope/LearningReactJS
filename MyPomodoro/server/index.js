5f8aa86f6d6f5f69508f6e723b89e7b7a12fd1f6
David <rodpe.david@gmail.com>
2021-03-25 08:01:58 -0600

    First backend setup completed
    First backend implementation completed

--- MyPomodoro/server/index.js
+++ MyPomodoro/server/index.js
@@ -1,14 +1,48 @@
-const http = require('http');
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const keys = require('./config/keys');
+// require('./models/User');
+// require('./models/Survey');
+// require('./services/passport');
+// mongoose.connect(keys.mongoURI);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+const app = express();
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.use(bodyParser.json());
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// require('./routes/authRoutes')(app);
+// require('./routes/billingRoutes')(app);
+// require('./routes/surveyRoutes')(app);
+
+if (process.env.NODE_ENV === 'production'){
+    //Express will serve up production assets
+    //like our main.js file, or main.css file
+    app.use(express.static('client/build'));
+
+    //Express will serve up the index.html file
+    //if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
+
